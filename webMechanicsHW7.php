4488e076bf4b7ec83c53632927034c56488c391f
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-03-01 17:12:30 +0200

    hw exercise 7, server request method redirect

---
+++ webMechanicsHW7.php
@@ -0,0 +1,39 @@
+<?php
+
+_d($_GET);
+$color = 'fff';
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $color = '#005B4D';
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $color = '#E6D262';
+    header('Location: http://localhost/antras/WebMechanicsHW7.php');
+    exit;
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get & Post + page redirect</title>
+</head>
+<body style="background-color: <?= $color ?>;">
+
+    <h1>Please press either button to change the colour of the page.</h1>
+    
+    
+    <form action="" method="get">
+    <button type="submit">GET</button>
+    </form>
+    <form action="" method="post">
+    <button type="submit">POST</button>
+    </form>
+    
+
+</body>
+</html>
