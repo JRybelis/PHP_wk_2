7a4095d57d720be97266f8d59912982d2bd7d7d2
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-03-01 09:34:18 +0200

    exercise 4 - redirect

---
+++ webMechanicsHW4Lemon.php
@@ -0,0 +1,19 @@
+<?php
+
+header('Location: http://localhost/antras/WebMechanicsHW4Orange.php'); //redirects 
+exit;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lemon</title>
+</head>
+<body style="background-color: #E6D262">
+    <h1>Yuzu</h1>
+</body>
+</html>
