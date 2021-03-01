a2fa7fcfa02938b7b37c8c34c2bdab0cdcd9c38b
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-03-01 06:38:39 +0200

    initial commit

---
+++ webMechanics.php
@@ -0,0 +1,38 @@
+<?php
+if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
+
+    $x = (float) ($_POST['x'] ?? 0); // default value for x = 0, if it was not set or was removed by user. Type cast to float type.  
+    $y = (float) ($_POST['y'] ?? 0);
+
+    echo sha1('katukas');
+    $sum = $x + $y;
+    setcookie('Result', $sum);
+
+    header('Location: http://localhost/antras/webMechanics.php');
+    die;
+}
+
+if (isset($_COOKIE['Result'])) {
+    $result = $_COOKIE['Result'];
+    setcookie('Result', '', time() - 5);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sum of two values</title>
+</head>
+<body>
+    <h1>Sum of two values</h1>
+    <h3>Result: <?= $result ?? '' ?></h3> 
+    <form action="http://localhost/antras/webMechanics.php" method="post">
+    <input type="text" name="x">
+    <input type="text" name="y">
+    <button type="submit">Sum</button>
+    </form>
+</body>
+</html>
